c604a0367479e6b963d4ca8b33515f101d9964b4
Peregon <peregonbw@gmail.com>
2019-09-04 05:41:17 +0300

    Updates

---
+++ precache-manifest.982c7aafd74d2a09a03db85e2427a163.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8604910d886e9eb890d2c9b2fb4a92f",
+    "url": "/React-SocialNetwork/index.html"
+  },
+  {
+    "revision": "61c4cf41b0b267eb9828",
+    "url": "/React-SocialNetwork/static/css/main.d15fa588.chunk.css"
+  },
+  {
+    "revision": "bce5e0a8816e63081e58",
+    "url": "/React-SocialNetwork/static/js/2.9d54572f.chunk.js"
+  },
+  {
+    "revision": "61c4cf41b0b267eb9828",
+    "url": "/React-SocialNetwork/static/js/main.8a6f043e.chunk.js"
+  },
+  {
+    "revision": "d5676b975d81e162e3ce",
+    "url": "/React-SocialNetwork/static/js/runtime~main.ed9820ea.js"
+  },
+  {
+    "revision": "05d1a99a91a5552d048ca2f15505fdb7",
+    "url": "/React-SocialNetwork/static/media/OpenSans-Bold.05d1a99a.woff"
+  },
+  {
+    "revision": "0fc8bb5af95997edafd9e975b3eb3ed7",
+    "url": "/React-SocialNetwork/static/media/OpenSans-Bold.0fc8bb5a.ttf"
+  },
+  {
+    "revision": "55bfb3e43457ec893f128c2d73f76457",
+    "url": "/React-SocialNetwork/static/media/OpenSans-Bold.55bfb3e4.eot"
+  },
+  {
+    "revision": "b599eef6c617ab38cba5f0f73c77dab2",
+    "url": "/React-SocialNetwork/static/media/OpenSans-Extrabold.b599eef6.ttf"
+  },
+  {
+    "revision": "cee4f07ed7988e917b69db983549fe65",
+    "url": "/React-SocialNetwork/static/media/OpenSans-Extrabold.cee4f07e.woff"
+  },
+  {
+    "revision": "fa25da4ad6b2e2e9484c771d7943deb1",
+    "url": "/React-SocialNetwork/static/media/OpenSans-Extrabold.fa25da4a.eot"
+  },
+  {
+    "revision": "5299020bf84d03883fa452ee7478c60f",
+    "url": "/React-SocialNetwork/static/media/OpenSans-Light.5299020b.ttf"
+  },
+  {
+    "revision": "87051a9d79eb9fc8c317b5777320f78d",
+    "url": "/React-SocialNetwork/static/media/OpenSans-Light.87051a9d.woff"
+  },
+  {
+    "revision": "e1d396939867c493f06f2bbc071993d9",
+    "url": "/React-SocialNetwork/static/media/OpenSans-Light.e1d39693.eot"
+  },
+  {
+    "revision": "22d4a36b7d366b2b213801c716dfb0b7",
+    "url": "/React-SocialNetwork/static/media/OpenSans-Semibold.22d4a36b.ttf"
+  },
+  {
+    "revision": "4c7b8237f45b448efb609f5d8e3997a9",
+    "url": "/React-SocialNetwork/static/media/OpenSans-Semibold.4c7b8237.woff"
+  },
+  {
+    "revision": "8165c1ec52bb9262b2105edc8032a5db",
+    "url": "/React-SocialNetwork/static/media/OpenSans-Semibold.8165c1ec.eot"
+  },
+  {
+    "revision": "10ba07da64cc6a490da4ac3af4dd238b",
+    "url": "/React-SocialNetwork/static/media/OpenSans.10ba07da.ttf"
+  },
+  {
+    "revision": "885e3d6f49fb6783494654e69793e802",
+    "url": "/React-SocialNetwork/static/media/OpenSans.885e3d6f.eot"
+  },
+  {
+    "revision": "90327f5ac43d0fbc708f3653ffc0a043",
+    "url": "/React-SocialNetwork/static/media/OpenSans.90327f5a.woff"
+  },
+  {
+    "revision": "2cfe592cac88044edc3e9a78251228a9",
+    "url": "/React-SocialNetwork/static/media/icomoon.2cfe592c.woff"
+  },
+  {
+    "revision": "8413830bee8f6de34b224474ffad15da",
+    "url": "/React-SocialNetwork/static/media/icomoon.8413830b.eot"
+  },
+  {
+    "revision": "adc0025117317ba53492f5bdd2b9d5ad",
+    "url": "/React-SocialNetwork/static/media/icomoon.adc00251.svg"
+  },
+  {
+    "revision": "b3170fec0edb0de802f61b4c0dff526e",
+    "url": "/React-SocialNetwork/static/media/icomoon.b3170fec.ttf"
+  }
+]);
